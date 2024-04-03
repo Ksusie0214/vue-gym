6017905af0e6f11bc800205e59f303bd82e917ce
Ksusie0214 <tdlrtmvldj@gmail.com>
2024-04-03 16:53:48 +0900

    로그인 css 완료

---
+++ src/views/JoinOkView.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div id="header">
+            <span>FITNESS ON</span>
+            <button id="HomeBtn">Home</button>
+            <button id="LogOutBtn">Logout</button>
+        </div>
+        <div id="JoinOkAll">
+            <h1 id="JoinOktitle1">회원가입을</h1>
+            <h1 id="JoinOktitle2">축하합니다!</h1>
+            <img src="../assets/img/fitness-icon.png" alt="" id="JoinOkImg">
+            <button id="JoinOkBtn">로그인으로</button>
+            <span class="footer1">CodeCrafters</span>
+            <span class="footer2">copyrights(c) all rights Reserved</span>
+        </div>
+    </div>
+</template>
+
+
+<script>
+import "@/assets/JoinOk.css"
+export default {
+    name: "JoinOkView",
+    components: {},
+    data() {
+        return {};
+    },
+    methods: {},
+};
+</script>
+
+
+<style></style>
